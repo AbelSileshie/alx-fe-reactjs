5f03e3e92d4586c2bffa7ea977322bc44c5d12d5
abelsileshie <sileshieabel@gmail.com>
2024-11-10 21:36:02 +0300

    fixed

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import React, { useContext } from "react";
+import UserContext from "./UserContext
 const UseProfile = () => {
   return (
     <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
