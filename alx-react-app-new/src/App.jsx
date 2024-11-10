db8041d9fa0caa79919b01fe9a61aca8292ad8a2
abelsileshie <sileshieabel@gmail.com>
2024-11-10 21:10:37 +0300

    added the nessary files

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Counter />
