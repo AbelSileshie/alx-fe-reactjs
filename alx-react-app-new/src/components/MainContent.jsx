1ab08b8c2fa537958715b55dfd5b844e23a94123
abelsileshie <sileshieabel@gmail.com>
2024-11-10 21:13:09 +0300

    fixed the main conent.jsx file d

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const MainContent = () => {
-  return <div>MainContent</div>;
+  return (
+    <>
+      <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </>
+  );
 };
 
 export default MainContent;
