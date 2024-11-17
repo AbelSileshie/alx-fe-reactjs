ec556b03bef28138fd3c34aed9b8b98ea77c500d
Abelsileshie <sileshieabel@gmail.com>
2024-11-17 21:28:20 +0300

    implemented a usenavigate back to App After Deletion

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,17 @@
 import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+    navigate("/");
+  };
 
   return (
-    <button onClick={() => deleteRecipe(recipeId)} style={{ color: "red" }}>
+    <button onClick={handleDelete} style={{ color: "red" }}>
       Delete Recipe
     </button>
   );
