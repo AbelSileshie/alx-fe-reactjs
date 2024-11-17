a82e97e5a955b325581d9a0c9cb3dc0255efda8c
Abelsileshie <sileshieabel@gmail.com>
2024-11-17 21:35:28 +0300

    added the routes in the router part for the error

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RecipeDetails from "./components/RecipeDetails";
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <AddRecipeForm />
-              <RecipeList />
-            </div>
-          }
-        />
-        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <AddRecipeForm />
+                <RecipeList />
+              </div>
+            }
+          />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
