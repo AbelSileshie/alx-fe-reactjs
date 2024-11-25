d5e5d51acd22be8e4bf63f96c5bb6f39a5648bae
Abelsileshie <sileshieabel@gmail.com>
2024-11-25 11:01:08 +0300

    added a tailwind css to my normal react code

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex text-blue-500">
+        <p className=" font-extrabold mt-12 p-12">
+          this is an example of tailwindcss
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
