867834482fb93f5b677e4d2fa5a9f5ff80ffec9c
Abel Sileshie <silesheabel@gmail.com>
2024-12-08 22:52:21 +0300

    fixed

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import recipeData from "../data.json";
 
 const HomePage = () => {
@@ -27,12 +28,12 @@ const HomePage = () => {
             <div className="p-4">
               <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
               <p className="text-gray-700 mb-4">{recipe.summary}</p>
-              <a
-                href={`/recipe/${recipe.id}`}
+              <Link
+                to={`/recipe/${recipe.id}`}
                 className="text-blue-500 hover:underline"
               >
                 View Recipe
-              </a>
+              </Link>
             </div>
           </div>
         ))}
